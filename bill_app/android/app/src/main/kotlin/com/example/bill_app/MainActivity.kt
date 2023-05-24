90bd8e0bc240661824e86064d799e23b655a3c74
dangthibichlai <119518565+dangthibichlai@users.noreply.github.com>
2023-05-24 13:31:01 +0700

    Trước khi sharePerfrences list bill

--- bill_app/android/app/src/main/kotlin/com/example/bill_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bill_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- bill_app/lib/models/function-home.dart
+++
@@ -1,7 +0,0 @@
-// ignore: file_names
-class FunctionHome {
-  int? id;
-  String? name;
-  String? image;
-  FunctionHome({this.id, this.name, this.image});
-}
